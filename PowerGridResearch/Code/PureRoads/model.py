f9163d3767b629f77e4f477948e29d11349b6c68
Brian French <bcfrench@utexas.edu>
2019-03-29 16:41:14 -0500

    Update model.py

--- PowerGridResearch/Code/PureRoads/model.py
+++ PowerGridResearch/Code/PureRoads/model.py
@@ -5,3 +5,27 @@ Created on Fri Mar 29 12:41:58 2019
 @author: BrianFrench
 """
 
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+import pyomo.environ as pe
+#initialize graph from file
+Grid = nx.read_gml("Bus30WithData.gml")
+Grid = nx.convert_node_labels_to_integers(Grid)
+#declare needed constants
+SteadyStatePower = 255 #in MW--the PU Basis
+PlanningHorizon = 12 #this is measured in shifts
+ShiftLength = 8 #in Hours
+#Define sets to be used in optimiation
+PowerSub = nx.read_gml("Bus30WithData.gml")
+PowerSub = nx.convert_node_labels_to_integers(PowerSub)
+for i in PowerSub.nodes:
+  for j in PowerSub.nodes:
+    if PowerSub.has_edge(i,j,1):
+        PowerSub.remove_edge(i,j,1)
+Edges = pe.Set(initialize = range(0,len(PowerSub.edges)))
+Nodes = pe.Set(initialize= range(0,30))
+Time = pe.Set(initialize = range(0,PlanningHorizon))
+model = pe.ConcreteModel()
