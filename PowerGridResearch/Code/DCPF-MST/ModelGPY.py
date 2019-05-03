a861c9a4392dc6b8ddebcb7146cf00ba269edac7
Brian French <bcfrench@utexas.edu>
2019-05-03 15:55:17 -0500

    IT WORKS

--- PowerGridResearch/Code/DCPF-MST/ModelGPY.py
+++ PowerGridResearch/Code/DCPF-MST/ModelGPY.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
-import pyomo.environ as pe
+from gurobipy import *
 #define a powerset function
 from itertools import chain, combinations
 def powerset(iterable):
@@ -22,7 +22,7 @@ Grid = nx.read_gml("Bus30WithData.gml")
 Grid = nx.convert_node_labels_to_integers(Grid)
 #declare needed constants
 SteadyStatePower = 255 #in MW--the PU Basis
-PlanningHorizon = 12 #this is measured in shifts
+PlanningHorizon = 6 #this is measured in shifts
 ShiftLength = 8 #in Hours
 #Define sets to be used in optimiation
 PowerSub = nx.read_gml("Bus30WithData.gml")
@@ -31,25 +31,25 @@ for i in PowerSub.nodes:
   for j in PowerSub.nodes:
     if PowerSub.has_edge(i,j,1):
         PowerSub.remove_edge(i,j,1)
-Edges = pe.Set(initialize = range(0,len(PowerSub.edges)))
-Nodes = pe.Set(initialize= range(0,30))
+Edges = list(range(0,len(PowerSub.edges)))
+Nodes = list(range(0,30))
 sumlen = 71
 #sumlen = len(Nodes)+len(Edges)
-NodesWithDummy = pe.Set(initialize = range(0,sumlen))
-Time = pe.Set(initialize = range(0,PlanningHorizon))
-model = pe.ConcreteModel()
+NodesWithDummy = list(range(0,sumlen))
+Time = list(range(0,PlanningHorizon))
+model = Model("mip1")
 #define Variables
 #Decision Variables
-model.PG = pe.Var(Nodes,Time)
-model.F_l = pe.Var(Edges,Time, domain=pe.Binary)
-model.F_n = pe.Var(Nodes,Time, domain = pe.Binary)
-model.Z = pe.Var(NodesWithDummy,NodesWithDummy,Time, domain = pe.Binary) #abuse total unimodularity to speed up the solving time if needed
+PG = model.addVars(Nodes,Time,vtype=GRB.CONTINUOUS, name = "PG",lb = 0)
+F_l = model.addVars(Edges,Time, vtype=GRB.BINARY, name = "F_l")
+F_n = model.addVars(Nodes,Time, vtype= GRB.BINARY, name = "F_n")
+Z = model.addVars(NodesWithDummy,NodesWithDummy,Time, vtype = GRB.BINARY, lb = 0, ub=1, name = "Z") #abuse total unimodularity to speed up the solving time if needed
 ##State Variables
-model.W_l = pe.Var(Edges,Time, domain = pe.Binary)
-model.W_n = pe.Var(Nodes, Time, domain = pe.Binary)
-model.Theta = pe.Var(Nodes,Time)
-model.PowerIJ = pe.Var(Edges,Time, domain = pe.Reals)
-model.MST = pe.Var(Time, domain = pe.NonNegativeReals)
+W_l = model.addVars(Edges,Time, vtype = GRB.BINARY, name = "W_l")
+W_n = model.addVars(Nodes, Time, vtype = GRB.BINARY, name = "W_n")
+Theta = model.addVars(Nodes,Time, vtype = GRB.CONTINUOUS, name = "Theta")
+PowerIJ = model.addVars(Edges,Time, vtype = GRB.CONTINUOUS, name = "PowerIJ")
+MST = model.addVars(Time, vtype = GRB.CONTINUOUS, lb=0, name = "MST")
 #default everything to working
 for i in Nodes:
     nx.set_node_attributes(Grid, {i:True},'working')
@@ -93,6 +93,7 @@ Grid[11][15][0]['working']=False
 Grid[1][3][0]['working']=False
 Grid[19][18][0]['working']=False
 ###END SCENARIO###            
+
 EdgeTracker = [] #this is an index i connected to a tuple where element 1 is the origin and element 2 is the destination
 for i,e in enumerate(PowerSub.edges):
     EdgeTracker.append([i,e])
@@ -104,23 +105,32 @@ for i in Nodes:
             for k in range(0,len(EdgeTracker)):
                 if EdgeTracker[k][1][0] == i and EdgeTracker[k][1][1]==j:
                     EdgeStartingStatus[k] = Grid[i][j][0]['working']
-model.obj = pe.Objective(expr = sum((1-model.W_n[i,t])*Grid.node[i]['load'] for i in Nodes for t in Time))
+###Build broken elements into a list for STE constraints####
+STE = []
+for i in range(len(Grid.nodes())):
+    if Grid.node[i]['working']==False:
+        STE.append(i)
+for j in range(len(EdgeStartingStatus)):
+    if EdgeStartingStatus[j]==0:
+        STE.append(30+j)
+###End STE building###
+
+
+obj = model.setObjective(sum((1-W_n[i,t])*Grid.node[i]['load'] for i in Nodes for t in Time),GRB.MINIMIZE)
 
 
 #impose phase angle constraints
 
 M=1000
-model.PAcons = pe.ConstraintList()
 for i in Nodes:
     for j in Nodes:
         for t in Time:
          for k in range(0,len(EdgeTracker)):
             if EdgeTracker[k][1][0] == i and EdgeTracker[k][1][1]==j:
-                    model.PAcons.add(model.PowerIJ[k,t] == Grid[i][j][0]['Sus']*(model.Theta[i,t]-model.Theta[j,t]))
-        model.PAcons.add(model.Theta[i,t]<=3.14)
-        model.PAcons.add(model.Theta[j,t]>=0)
+                    model.addConstr(PowerIJ[k,t] == Grid[i][j][0]['Sus']*(Theta[i,t]-Theta[j,t]))
+        model.addConstr(Theta[i,t]<=3.14)
+        model.addConstr(Theta[j,t]>=0)
 #impose power balance constraints
-model.PBcons = pe.ConstraintList()
 for i in Nodes:
     for t in Time:
       originadj = []
@@ -130,39 +140,37 @@ for i in Nodes:
               originadj.append(k)
           if EdgeTracker[k][1][1]==i:
               destadj.append(k)
-      model.PBcons.add(model.PG[i,t]-sum(model.PowerIJ[j,t] for j in originadj)+sum(model.PowerIJ[k,t] for k in destadj) == Grid.node[i]['load']*model.W_n[i,t])
+      model.addConstr(PG[i,t]-sum(PowerIJ[j,t] for j in originadj)+sum(PowerIJ[k,t] for k in destadj) == Grid.node[i]['load']*W_n[i,t])
 #constrain maximum power generation and handle functionality of the node
 for i in Nodes:
     for t in Time:
-        model.PBcons.add(model.PG[i,t]<=Grid.node[i]['productionmax']*model.W_n[i,t])
-        model.PBcons.add(model.PG[i,t]>=0)
+        model.addConstr(PG[i,t]<=Grid.node[i]['productionmax']*W_n[i,t])
+        model.addConstr(PG[i,t]>=0)
 #constrain line limits
-model.LineLoadcons = pe.ConstraintList()
 for e in Edges:
         for t in Time:
-                model.LineLoadcons.add(model.PowerIJ[e,t]<=Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*model.W_l[e,t])     
-                model.LineLoadcons.add(model.PowerIJ[e,t]<=Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*model.W_n[EdgeTracker[e][1][0],t])     
-                model.LineLoadcons.add(model.PowerIJ[e,t]<=Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*model.W_n[EdgeTracker[e][1][1],t])
-                model.LineLoadcons.add(model.PowerIJ[e,t]>=-1*Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*model.W_l[e,t])     
-                model.LineLoadcons.add(model.PowerIJ[e,t]>=-1*Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*model.W_n[EdgeTracker[e][1][0],t])     
-                model.LineLoadcons.add(model.PowerIJ[e,t]>=-1*Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*model.W_n[EdgeTracker[e][1][1],t])                
+                model.addConstr(PowerIJ[e,t]<=Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*W_l[e,t])     
+                model.addConstr(PowerIJ[e,t]<=Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*W_n[EdgeTracker[e][1][0],t])     
+                model.addConstr(PowerIJ[e,t]<=Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*W_n[EdgeTracker[e][1][1],t])
+                model.addConstr(PowerIJ[e,t]>=-1*Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*W_l[e,t])     
+                model.addConstr(PowerIJ[e,t]>=-1*Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*W_n[EdgeTracker[e][1][0],t])     
+                model.addConstr(PowerIJ[e,t]>=-1*Grid[EdgeTracker[e][1][0]][EdgeTracker[e][1][1]][0]['capacity']*W_n[EdgeTracker[e][1][1],t])                
            
 
 #define workingness
-model.Working = pe.ConstraintList()
 for i in Nodes:
-    model.Working.add(model.W_n[i,0] <= Grid.node[i]['working'])
+    model.addConstr(W_n[i,0] <= Grid.node[i]['working'])
     for t in range(1,len(Time)):
-        model.Working.add(model.W_n[i,t]<=sum(model.F_n[i,g] for g in range(0,t))+Grid.node[i]['working'])
+        model.addConstr(W_n[i,t]<=sum(F_n[i,g] for g in range(0,t))+Grid.node[i]['working'])
 for e in Edges:
     for t in Time:
-        model.Working.add(model.W_l[e,t]<=sum(model.F_l[e,g] for g in range(0,t))+EdgeStartingStatus[e])
+        model.addConstr(W_l[e,t]<=sum(F_l[e,g] for g in range(0,t))+EdgeStartingStatus[e])
         
-##schedule restriction so that nothing gets double fixed
-#for i in Nodes:
-#    model.Working.add(sum(model.F_n[i,t]for t in Time)<=1)   
-#for e in Edges:
-#    model.Working.add(sum(model.F_l[e,t]for t in Time)<=1)         
+#schedule restriction so that nothing gets double fixed
+for i in Nodes:
+    model.addConstr(sum(F_n[i,t]for t in Time)<=1)   
+for e in Edges:
+    model.addConstr(sum(F_l[e,t]for t in Time)<=1)         
 #build shortest path matrix
 SP = np.zeros((len(NodesWithDummy), len(NodesWithDummy)))
 RoadGrid = nx.Graph()
@@ -178,43 +186,23 @@ for i,e in enumerate(EdgeTracker):
 for i in NodesWithDummy:
     for j in NodesWithDummy:
         SP[i][j] = nx.shortest_path_length(RoadGrid, source = i, target = j, weight='weight')
-model.MSTCons = pe.ConstraintList()
 for t in Time:
-    model.MSTCons.add(model.MST[t] == sum(SP[i][j]*model.Z[i,j,t] for i in NodesWithDummy for j in NodesWithDummy))
-    model.MSTCons.add(sum(model.Z[i,j,t] for i in NodesWithDummy for j in NodesWithDummy) == sum(model.F_n[i,t]for i in Nodes)+sum(model.F_l[e,t] for e in Edges)-1)
-    for s in powerset(NodesWithDummy):
+    model.addConstr(MST[t] == sum(SP[i][j]*Z[i,j,t] for i in NodesWithDummy for j in NodesWithDummy))
+    model.addConstr(sum(Z[i,j,t] for i in NodesWithDummy for j in NodesWithDummy) == sum(F_n[i,t]for i in Nodes)+sum(F_l[e,t] for e in Edges)-1)
+    for s in powerset(STE):
         if len(s)>=2:
-            model.MSTCons.add(sum(model.Z[i,j,t] for i in s for j in s)<=len(s)-1)
+            model.addConstr(sum(Z[i,j,t] for i in s for j in s)<=len(s)-1)
     for i in Nodes:
 #        dropi = Nodes
 #        dropi.remove(i)
-       model.MSTCons.add(sum(model.Z[i,j,t] for j in Nodes)>=model.F_n[i,t])
+       model.addConstr(sum(Z[i,j,t] for j in Nodes)>=F_n[i,t])
     for e in Edges:
         i = EdgeTracker[e][1][0]
         j = EdgeTracker[e][1][1]
         E = len(Nodes)+e
-        model.MSTCons.add(sum(model.Z[E,j,t] for j in NodesWithDummy) <= model.F_l[e,t])
+        model.addConstr(sum(Z[E,j,t] for j in NodesWithDummy) <= F_l[e,t])
 #arbitrarily assigning node 13 to be the warehouse node
-model.Scheduling = pe.ConstraintList()
 for t in Time:
-    model.Scheduling.add(sum(model.F_n[i,t]*5 for i in Nodes)+sum(model.F_l[e,t]*1 for e in Edges)+model.MST[t]<=8)
+    model.addConstr(sum(F_n[i,t]*5 for i in Nodes)+sum(F_l[e,t]*1 for e in Edges)+MST[t]<=8)
 
-solver = pe.SolverFactory('cplex')
-results = solver.solve(model, tee=True)
-print(results)    
-for t in Time:
-    for i in Nodes:
-        if model.W_n[i,t].value <1:
-            print(["Wtime",t,"node",i])
-for t in Time:
-    for e in Edges:
-        if model.W_l[e,t].value <1:
-            print(["Wtime",t,"edge",e])
-for t in Time:
-    for i in Nodes:
-        if model.F_n[i,t].value==1:
-            print(['Ftime',t,"node",i])
-    for e in Edges:
-        if model.F_l[e,t].value==1:
-            print(['Ftime',t,"edge",e])
-    
+model.optimize()
